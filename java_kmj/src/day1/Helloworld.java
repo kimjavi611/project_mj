2f035e9a3f65b853ce3138a882ce98e52792e4b7
kimjavi611 <kimjavi611@gmail.com>
2021-04-26 17:34:25 +0900

    자바1일차 주석 설명 예제

---
+++ java_kmj/src/day1/Helloworld.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Helloworld {
+/* 작성일 : 2021-04-26
+ * 작성자 : 김민지
+ * 내용 : 기본 프로젝트 생성 및 테스트
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld!");
+		
+
+	}
+
+}
