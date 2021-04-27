6bc3182c50ab455d3faff1806fd2b59527910119
kimjavi611 <kimjavi611@gmail.com>
2021-04-28 01:02:15 +0900

    자바2일차 if문 예제

---
+++ java_kmj/src/day2/if1.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class if1 {
+
+	public static void main(String[] args) {
+		// if문 예제 1
+		int num = 1;
+		
+		if(num == 0) {
+			System.out.println("0입니다.");
+		} else if(num == 1) {
+			System.out.println("1입니다.");
+		}  else {
+			System.out.println("0과 1이 아닙니다.");
+			
+		}
+
+	}
+
+}
