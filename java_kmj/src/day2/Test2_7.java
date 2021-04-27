e81f3c15f3fec3ce5065dd6ba90a51d12a273439
kimjavi611 <kimjavi611@gmail.com>
2021-04-28 02:07:00 +0900

    자바2일차 Test2_7 수정

--- java_kmj/src/day2/Test2_7.java
+++ java_kmj/src/day2/Test2_7.java
@@ -13,15 +13,17 @@ public class Test2_7 {
 		 * 2월은 28일까지 있습니다.
 		 * */
 		
-		int month = 2;
+		int month = 11;
+		if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			System.out.println(month + "월은 31일까지 있습니다. ");
+		}else if(month == 2) {
+			System.out.println(month + "월은 28일까지 있습니다. ");
+		}if(month == 4 || month == 6 || month == 9 || month == 11) {
+			System.out.println(month + "월은 30일까지 있습니다. ");
+		}
 		
 	
-		if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 ) {
-			System.out.println(month + "월은 31일까지 있습니다.");
-		}else if(month == 2) {
-			System.out.println(month + "월은 28일까지 있습니다.");
-			if(month == 4 || if(month == 2) {
-				System.out.println(month + "월은 28일까지 있습니다.");
+
 			
 		
 	}
