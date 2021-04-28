a401af9b9230be1748fca25b5d425602af365f57
kimjavi611 <kimjavi611@gmail.com>
2021-04-28 17:32:01 +0900

    자바3일차 for문 예제5

---
+++ java_kmj/src/day3/for5.java
@@ -0,0 +1,25 @@
+package day3;
+
+public class for5 {
+
+	public static void main(String[] args) {
+		/* 1부터 5까지의 합을 구하는 예제
+		 *  반복횟수 : i는 1부터 5까지 1씩 증가
+		 *  규칙성 : sum = sum + i를 실행(sum += i)
+		 *  		sum = 0
+		 *  i = 1	sum = sum0 + 1
+		 *  i = 2	sum = sum1 + 2
+		 *  i = 3 	sum = sum2 + 3
+		 *  i = 4	sum = sum3 + 4
+		 *  i = 5	sum = sum4 + 5
+		 *  */
+		int i; 
+		for(i=1 ; i <= 5 ; i++) {
+			sum += i;
+			System.out.println("1부터 5까지의 합 : " + sum);
+		}
+
+		
+	}
+
+}
