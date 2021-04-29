ab61209f47d1305eabded6e1555e48b8b6235520
kimjavi611 <kimjavi611@gmail.com>
2021-04-29 13:05:03 +0900

    자바4일차 for문 예제11

---
+++ java_kmj/src/day4/For11.java
@@ -0,0 +1,41 @@
+package day4;
+
+public class For11 {
+
+	public static void main(String[] args) {
+		/* 두 정수의 최소 공배수를 구하는 코드를 작성하세요.
+		 * 8의 배수 : 8, 16, 24, 32, ...
+		 * 12의 배수 : 12, 24, 36, 48, ...
+		 * 8과 12의 공배수 : 24, 48, 72, ...
+		 * 8과 12의 최소 공배수 : 24
+		 * 반복횟수 :
+		 * 			i는 1부터 두 수의 곱까지 1씩 증가
+		 * 규칙성 :i가 num1의 배수이고, i가 num2의 배수면 i를 출력 후 반복문 종료
+		 * 			=> i를 num1로 나누었을 떄 나머지가 0과 같고
+		 * 				i를 num2로 나누었을 때 나머지가 0과 같다면
+		 * 				i를 출력 후 반복문 종료
+		 * 반복문 종료 후 : 없음
+		 * */
+		int i;
+		int num1 = 8;
+		int num2 = 12;
+		int gcd = 1;
+		for( i = 1 ; i <= num1 * num2 ; i += 1 ) {
+			if(i % num1 == 0 && i % num2 == 0) {
+			System.out.println(i);
+			break;//최소 공배수는 가장 처음 출력 되었을떄의 값 => 참이 처음으로 나오고 나면 종료. 
+				
+			}
+		}
+		
+		/* 반복횟수 : i는 num1부투터 무한대로 i는 num1씩 증가( num1의 배수만큼)
+		 * 규칙성 : i가 num2의 배수이면 i를 출력 후 반복문 종료
+		 * */
+		for( i = num1 ; ; i += num1) {
+			if(1 % num2 == 0)
+			System.out.println(i);
+			break;
+		}
+	}
+
+}
