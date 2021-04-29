bb7b2b7bda1dab797f64abd11ce358066d362b19
kimjavi611 <kimjavi611@gmail.com>
2021-04-29 17:25:03 +0900

    자바3일차 Test모음

--- java_kmj/src/day3/Test3_6.java
+++ java_kmj/src/day3/Test3_6.java
@@ -5,7 +5,7 @@ public class Test3_6 {
 	public static void main(String[] args) {
 		/* 1부터 10까지의 짝수들의 합을 구하는 예제 */
 		
-		int i; sum = 0;
+		int i, sum = 0;
 		for(i = 2 ; i<= 10; i+=2) {
 			
 			//sum += 1;
