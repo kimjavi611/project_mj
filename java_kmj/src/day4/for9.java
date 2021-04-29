583c14a42901d8cb8a82c466112be86e47ce6822
kimjavi611 <kimjavi611@gmail.com>
2021-04-29 13:04:36 +0900

    자바4일차 for문 예제9

---
+++ java_kmj/src/day4/for9.java
@@ -0,0 +1,21 @@
+package day4;
+
+public class for9 {
+
+	public static void main(String[] args) {
+		/* 무한루프 : 반복문이 끝나지 않는 경우를 무한루프라 한다. 
+		 * */
+		int i = 1;
+		for( ;i < 5 ; ) {
+			System.out.println("Hello world!");
+		}
+		// 조건식이 있어도 계속 참이면 무한루프에 빠질 수 있다.
+		// 전부 생략된 경우 출력이 계속 된다 = 무한루프
+		
+		//한번도 실행 안되는 경우도 있다.
+	
+		
+
+	}
+
+}
