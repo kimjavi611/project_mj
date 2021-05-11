9524c689a9a42c3464031d1743050bad06edc324
mjkim <kimjavi611@gmil.com>
2021-05-11 09:16:56 +0900

    자바10일차 클래스 예제6

--- java_kmj/src/day10/ClassEx6.java
+++ java_kmj/src/day10/ClassEx6.java
@@ -14,7 +14,7 @@ public class ClassEx6 {
 		Point rd = new Point(20,0);
 		Rect r3 = new Rect(lu,rd);
 		r3.print();
-		lu.setX(-10);
+		lu.setX(-10);//점을 수정했더니 사각형의 점의 위치가 같이 수정되는 문제가 발생 
 		r3.print();
 	}
 
