ff0cbfd3b25336496e0046c6ad3ccf2e10f2c843
mjkim <kimjavi611@gmil.com>
2021-05-12 17:36:44 +0900

    자바11일차 게시판생성 예제

--- java_kmj/src/board/Board.java
+++ java_kmj/src/board/Board.java
@@ -29,6 +29,7 @@ public class Board {
 	 * 메소드명 : print
 	 * */
 	public void print() {
+		views +=1;
 		System.out.println("제목 : " + title);
 		System.out.println("작성자 : " + writer);
 		System.out.println("조회수 : " + views);
@@ -37,7 +38,28 @@ public class Board {
 		System.out.println("내용 : " + contents);
 		
 	}
-		
+	/* 게시글 정보를 요약 (번번호, 제목, 작성자, 작성일, 조회수)해서 출력하는 기능
+	 * 매개변수 : 없음
+	 * 리턴타입 : 없음 => void
+	 * 메소드명 : summaryPrint
+	 * */
+	public void summaryPrint() {
+			System.out.print(num + " ");
+			System.out.print(title + " ");
+			System.out.print(writer + " ");
+			System.out.print(registerDate + " ");
+			System.out.print(views + " ");
+			System.out.println();
+	}
+	/* 기능 : 제목과 내용을 수정하는 메소드
+	 * 매개변수 : 수정할 제목, 내용 => String title, String contents
+	 * 리턴타입 : 없음 =>void
+	 * 메소드명 : modify
+	 * */
+	public void modify(String title, String contents) {
+		this.title = title;
+		this.contents = contents;
+	}
 	
 	
 	
