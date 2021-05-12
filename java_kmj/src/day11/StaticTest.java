a7265aa81d3d8776b666fa90de41f67db3834b1d
mjkim <kimjavi611@gmil.com>
2021-05-12 17:39:29 +0900

    자바11일차 static 예제

--- java_kmj/src/day11/StaticTest.java
+++ java_kmj/src/day11/StaticTest.java
@@ -7,6 +7,7 @@ public class StaticTest {
 		// 객체 멤버 변수 num1을 호출 => 객체를 생성한 후 멤버변수를 호출할 수 있다
 		//객체 생성 없이 객체 멤버변수를 호출할 수 없다
 		TestB t1;
+		t1 = new TestB();
 		TestB t2 = new TestB();
 
 		t1.num1 =10;
@@ -33,11 +34,11 @@ class TestB{
 		num2 =20;
 		printNum2();
 	}//클래스 멤버변수/메소드는 객체 메소드에서 사용 가능
-	public void printNum2() {
+	public static void printNum2() {
 		//클래스 멤버변수는 클래스 메소드에서 사용 가능
 		System.out.println("num2 : " +num2);
 		//객체 멤버변수/ 메소드는 클래스 메소드에서 사용 불가능
-		num1 =10;//에러발생
-		printNum1();//에러발생
+		//num1 =10;//에러발생
+		//printNum1();//에러발생
 	}
 }
