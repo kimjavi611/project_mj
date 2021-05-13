1a1f42c62f34ac7785293d5eba90ccd7df114e7f
mjkim <kimjavi611@gmil.com>
2021-05-13 17:50:59 +0900

    가계부 클래스

--- java_kmj/src/accountbook/AccountBook.java
+++ java_kmj/src/accountbook/AccountBook.java
@@ -1,10 +1,39 @@
 package accountbook;
+/* 가계부 프로그램을 구현하려고 한다. 이 때 필요한 클래스를 만들어 보세요
+ * */ 
+import java.util.Scanner;
 
 public class AccountBook {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//카테고리 제공
+		do {
+			System.out.println("1. 날짜입력");
+			System.out.println("2. 수입,지출 기록");
+			System.out.println("3. ");
+		}while();
+		
+		//날짜 입력 받음
+		System.out.println("기록할 날짜를 입력하세요 : ");
+		Scanner scan = new Scanner(System.in);
+		int date =scan.nextInt();
+		scan.close();
+		
 	}
-
+	
+	
 }
+
+
+//가계부 => 카테코리  
+//날짜, 수입 금액, 수입 내용, 지출 금액, 지출 내용, 총수입액, 총지출액, 잔액 표시 필요
+// 기록할 날짜 입력받음
+//수입,지출 기록 선택
+//분류 : 식비/ 교통 / 건강
+//금액 입력
+//내용 입력
+//총 금액 합산, 출력
+// 잔액 표시(원래있던거 + 총 수입 -총 지출)
+
+//클래스 필요? =>  
--- java_kmj/src/day13/PolymorphismTest.java
+++ java_kmj/src/day13/PolymorphismTest.java
@@ -21,16 +21,27 @@ public class PolymorphismTest {
 	 * 리턴타입 : 이상여부 => 참 거짓 => boolean
 	 * 메소드명 : confirm
 	 * */
-	
+	//매개변수의 다형성을 확인하는 예제
+	//instanceof를 이용하면 매개변수의 다형성을 처리하기가 쉬워진다.
 	public static boolean confirm(Tire t) {
 		//타이어 t가 한국타이어면 예외가 발생하지 않고
 		//금호타이어면 예외가 발생함
-		HankookTire ht =(HankookTire)t; 
+		//HankookTire ht =(HankookTire)t;
+		if(t instanceof HankookTire){
+			HankookTire ht =(HankookTire)t;
+			//한국 타이어 검사하는 코드 작성했다고 가정
+			System.out.println("한국 타이어 검사 끝!");
+		}else if(t instanceof KumhoTire) {
+			KumhoTire kt = (KumhoTire)t;
+			//금호타이어 검사하는 코드 작성했다고 가정
+			System.out.println("금호 타이어 검사 끝!");
+		}
 		return false;
 	}
 
 }
 class Car2{
+	//멤버변수(필드)의 다형성
 	public Tire frontLeftTire;
 	public Tire frontRightTire;
 	public Tire BackLeftTire;
