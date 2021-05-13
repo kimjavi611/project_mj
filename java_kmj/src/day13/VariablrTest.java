8cdc1fae1734dda4d61a6ee2c8b469926011cfe7
mjkim <kimjavi611@gmil.com>
2021-05-13 17:50:26 +0900

    자바13일차 변수

---
+++ java_kmj/src/day13/VariablrTest.java
@@ -0,0 +1,22 @@
+package day13;
+
+public class VariablrTest {
+
+	public static void main(String[] args) {
+		int num5 =0;//일반변수,지역변수
+		String str3 ="Hello";//참조변수,객체(클래스로 만들어진 변수),지역변수
+		int[]arr = new int[5];//참조변수,배열,지역변수
+		Board[] board = new Board[5];//참조변수,배열,지역변수
+	}
+
+}
+class A{
+	int num1;//일반변수,객체 멤버 변수
+	static int num2;//일반변수,클래스 멤버변수
+	String str1;//참조변수,객체,객체 멤버 변수
+	static String str2;//참조변수,객체, 클래스 멤버 변수
+	public A(int num3,int num4) {//일반변수,매개변수
+		num1 = num3;
+		num2 = num4;
+	}
+}
