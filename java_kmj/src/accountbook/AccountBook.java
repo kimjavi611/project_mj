2fa841145a9f3b8825d1fc6151c1fb6ed15b6f2f
mjkim <kimjavi611@gmil.com>
2021-05-14 17:35:49 +0900

    가계부 예제(나)

--- java_kmj/src/accountbook/AccountBook.java
+++ java_kmj/src/accountbook/AccountBook.java
@@ -1,26 +1,53 @@
 package accountbook;
 /* 가계부 프로그램을 구현하려고 한다. 이 때 필요한 클래스를 만들어 보세요
  * */ 
-import java.util.Scanner;
+
 
 public class AccountBook {
+	 //-수입 내역, 지출 내역을 관리하기 위한 가계부 클래스
+	//멤버변수 
+	//
+	//멤버메소드
+	/*메뉴를 알려주는 메소드 
+	 1. 수입내역 입력
+	 2. 지출내역 입력
+	 3. 잔액 확인
+	 4. 수정
+	 5. 종료
+	
 
-	public static void main(String[] args) {
-		
-		//카테고리 제공
-		do {
-			System.out.println("1. 날짜입력");
-			System.out.println("2. 수입,지출 기록");
-			System.out.println("3. ");
-		}while();
-		
-		//날짜 입력 받음
-		System.out.println("기록할 날짜를 입력하세요 : ");
-		Scanner scan = new Scanner(System.in);
-		int date =scan.nextInt();
-		scan.close();
+	*/
+	private Item [] arr;//가게부 내역들
+	private int count;//저장된 내역들 갯수
+	private int total;//내역에 기입된 금액의 총 양
+	//타입,날짜,결제방법,카테고리=> 선택하는거 입력받아야함=> 
+	/* 기능 : 내역이 주어지면 주어진 내역을 가계부에 저장하는 메소드
+	 * 매개변수 : 주어진 내역들 => String type, String date, String paymentType, String category, String contents, int cost  
+	 * 리턴타입 : 없음 => void
+	 * 메소드명 : accountList
+	 */
+	public void accountLIst(String type, String date, String paymentType, String category, String contents, int cost) {
 		
 	}
+	
+	 /*  기능 : 입력한 내역을 확인하는 메소드
+	 *  매개변수 : 입력된 가계부 내역들 => Item[]arr
+	 *  리턴타입 : 
+	 *  
+	 *  기능 : 입력한 내역을 수정하는 메소드
+	 *  
+	 *  기능 : 입력한 내역을 삭제하는 메소드
+	 *  
+	 *  기능 : 입력한 내역들의 총 금액(잔액?)을 알려주는 메소드
+	 *  
+	 *  기능 : */
+	
+	
+	
+	
+		
+		
+	
 	
 	
 }
