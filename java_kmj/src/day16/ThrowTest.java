608ecbd7650b8100dca6da76db76869bb8833789
mjkim <kimjavi611@gmil.com>
2021-05-18 17:45:57 +0900

    예외 처리 throw

---
+++ java_kmj/src/day16/ThrowTest.java
@@ -0,0 +1,27 @@
+package day16;
+
+public class ThrowTest {
+
+	public static void main(String[] args) {
+		int num = 1;
+		try {
+			if(num == 1 ) {
+				RuntimeException e = new RuntimeException("예외");
+				throw e;
+				//throw new RuntimeException("예외");
+				//자바에서 설정한 예외x 개발자가 원하는 시점에 예외 처리를 함
+			}
+		}catch(RuntimeException e) {
+			System.out.println("실행 예외가 발생");
+		}catch(Exception e) {
+			System.out.println("예외가 발생");
+		}
+		//Exception은 실행예외가 아니기 때문에 예외처리가 필수이다.
+		//예외 처리를 안한 아래 코드는 에러 발생
+		//throw new Exception("예외");
+		//RuntimeException은 실행 예외이기 때문에 예외 처리를 안해도 에러가 발생하지 않음
+		throw new RuntimeException("예외");
+	}
+		
+
+}
