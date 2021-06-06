f0ac7f8b7989b070e6497ac40fed5c94ab4ad94b
kimjavi611 <kimjavi611@gmail.com>
2021-06-07 03:37:42 +0900

    Create truncate예문.sql

---
+++ truncate예문.sql
@@ -0,0 +1,5 @@
+truncate table t2; 
+SELECT * FROM test.t2;
+insert into t2(contents)
+values('1'),('2'),('3'),('4'),('5'),('6'),('7'),('8'),('9'),('10'),('11'),('12'),('13'),('14'),('15');
+select * from t2;
