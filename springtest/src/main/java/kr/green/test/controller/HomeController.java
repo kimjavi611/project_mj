4f05cc0615c28be8aef90adfd6b5a913af9d658a
qoa1193 <cos1787@naver.com>
2021-07-06 12:24:19 +0900

    스프링테스트

--- springtest/src/main/java/kr/green/test/controller/HomeController.java
+++ springtest/src/main/java/kr/green/test/controller/HomeController.java
@@ -6,34 +6,31 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.green.test.service.MemberService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	@Autowired
+	MemberService memberService;
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public ModelAndView home(ModelAndView mv) {
+		mv.setViewName("home");
+		return mv;
 	}
-	
+	@RequestMapping(value = "/signin", method = RequestMethod.GET)
+	public ModelAndView signinGet(ModelAndView mv) {
+		mv.setViewName("signin");
+		return mv;
+	}
 }
