5d045b69b37f57a22c867dda39affd47d27622c3
qoa1193 <cos1787@naver.com>
2021-07-21 11:46:54 +0900

    스프링테스트 : 댓글 등록

---
+++ springtest/src/main/java/kr/green/test/service/ReplyService.java
@@ -0,0 +1,13 @@
+package kr.green.test.service;
+
+import java.util.ArrayList;
+
+import kr.green.test.vo.ReplyVO;
+
+public interface ReplyService {
+
+	void insertReply(ReplyVO reply);
+
+	ArrayList<ReplyVO> getReplyList(Integer num);
+
+}
--- springtest/src/main/java/kr/green/test/vo/ReplyVO.java
+++ springtest/src/main/java/kr/green/test/vo/ReplyVO.java
@@ -1,5 +1,8 @@
 package kr.green.test.vo;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,9 +10,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ReplyVO {
 	private int rp_num;
-	private int rp_db_num;
+	private int rp_bd_num;
 	private String rp_me_id;
 	private String rp_content;
 	private String rp_valid;
-	
+	private Date rp_regDate;
+	private Date rp_upDate;
+	 public String getRp_regDateStr() {
+		 SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		 return format.format(rp_regDate);
+	 }
+	 public String getRp_upDateStr() {
+		 SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		 return format.format(rp_upDate);
+	 }
 }
